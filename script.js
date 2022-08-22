a5ea5e2c5262adcd0866322aedb167a3c4afc44d
ctrl-Alx-del <electromanati@gmail.com>
2022-08-22 14:10:37 +0200

    opgave 2b done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 let sections = document.querySelector("section");
 
 console.log(sections);
@@ -30,4 +30,25 @@ console.log(article2);
 let article2H3 = document.querySelector("section:nth-child(2) " + "article")
 
 console.log(article2H3);
+*/
 
+
+let overskrift = document.querySelector("h1");
+
+overskrift.textContent = "Kageopskrift";
+
+let articleH3 = document.querySelector("article:nth-child(2)");
+
+articleH3.innerHTML = "Småkager";
+
+let article1 = document.querySelector("article:nth-child(3)");
+
+article1.textContent = "Sådan her laver du lækre småkager";
+
+let article2H3 = document.querySelector("section:nth-child(2) " + "article")
+
+article2H3.textContent = "Muffins";
+
+let article2 = document.querySelector("#article2");
+
+article2.innerHTML = "<b> Muuuuuuuuuuuffiiiiiiins </b>";
