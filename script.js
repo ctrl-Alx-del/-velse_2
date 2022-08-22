b1da24116e315e6c90b36f7c4f373390d79d029b
ctrl-Alx-del <electromanati@gmail.com>
2022-08-22 13:56:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let sections = document.querySelector("section");
+
+console.log(sections);
+
+let section2 = document.querySelector("section:nth-child(2)");
+
+console.log(section2);
+
+let overskrift = document.querySelector("h1");
+
+console.log(overskrift);
+
+let underoverskrift = document.querySelector("h2");
+
+console.log(underoverskrift);
+
+let articleH3 = document.querySelector("article:nth-child(2)");
+
+console.log(articleH3);
+
+let article1 = document.querySelector("article:nth-child(3)");
+
+console.log(article1);
+
+let article2 = document.querySelector("#article2");
+
+console.log(article2);
+
+let article2H3 = document.querySelector("section:nth-child(2) " + "article")
+
+console.log(article2H3);
+
